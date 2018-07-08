fe2c79d43b31025f3d99be80545617f0cac13fd3
alexismalzieu <alex.malzieu@gmail.com>
2018-07-08 15:24:08 +0200

    init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath))
+
+// app.get('/', function (req, res) {
+//   res.send('./../public/index.html');
+// })
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}`)
+})
