8117fc644890e4149b480773d3191fb0ee37d5d5
alexismalzieu <alex.malzieu@gmail.com>
2018-07-13 11:03:42 +0200

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,20 +15,14 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('welcomeMessage',generateMessage('Admin', 'Welcome to the chat app'));
-
-    socket.broadcast.emit('newUser',generateMessage('Admin', 'New user joined'));
-
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
         callback('This is from the server');
-        // socket.broadcast.emit('newMessage',{
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+    });
 
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 
@@ -10,5 +10,19 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).to.be.a('number');
         expect(message).to.include({from,text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+
+    it('should generate correct location object', () => {
+        let from = 'Jack';
+        let latitude = 123;
+        let longitude = 456;
+        let url = 'https://www.google.com/maps?q=123,456'
+        let message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).to.be.a('number');
+        expect(message).to.include({from, url});
     });
 });
