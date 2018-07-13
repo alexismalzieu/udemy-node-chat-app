c822bc77d1899f76894786363d4ff606e3c09316
alexismalzieu <alex.malzieu@gmail.com>
2018-07-13 17:50:55 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('newLocationMessage', function (message) {
 $('#message-form').on('submit',function (e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -45,12 +47,18 @@ locationButton.on('click',function () {
         return alert('Geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
         alert('Unable to fetch location.');
+        locationButton.removeAttr('disabled').text('Send location');
+
     });
 });
